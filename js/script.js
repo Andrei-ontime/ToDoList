9621ce4e78e36b59ef99ba10ada1ec1add46a13e
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-10-23 12:28:13 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -2,36 +2,35 @@ const input = document.querySelector('#input');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 
-
 //add Event
 
 btn.addEventListener('click', (e) => {
-	if (input.value === '') return
-	createDeleteElements(input.value)
-	input.value = ''
-})
+  if (input.value === '') return;
+  createDeleteElements(input.value);
+  input.value = '';
+});
 
 //create and delete ToDo
 function createDeleteElements(value) {
-	console.log(value)
+  console.log(value);
 
-	const li = document.createElement('li')
-	li.className = 'li'
-	li.textContent = value                                    
+  const li = document.createElement('li');
+  li.className = 'li';
+  li.textContent = value;
 
-	const btn = document.createElement('button')
-	btn.className = 'btn-delete'
-	btn.textContent = 'X'
-	li.appendChild(btn)
+  const btn = document.createElement('button');
+  btn.className = 'btn-delete';
+  btn.textContent = 'X';
+  li.appendChild(btn);
 
-//remove ToDo
-	btn.addEventListener('click', (e) => {
-		result.removeChild(li)
-	})
+  //remove ToDo
+  btn.addEventListener('click', (e) => {
+    result.removeChild(li);
+  });
 
-	li.addEventListener('click', (e) => {
-		li.classList.toggle('li-active')
-	})
+  li.addEventListener('click', (e) => {
+    li.classList.toggle('li-active');
+  });
 
-	result.appendChild(li)
+  result.appendChild(li);
 }
